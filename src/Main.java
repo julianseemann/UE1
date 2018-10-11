3a149923b1d2e621bf6a1a66cf4e5f54439beb06
Seemann Julian Max Georg <1810653188@stud.fh-kufstein.ac.at>
2018-10-11 11:00:20 +0200

    erster commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String vorname = "Julian";
+        Integer alter = 18.7;
+        System.out.println("Hello "+vorname+" du bist "+alter);
+
+    }
+}
